5938428ed863b12371f396fcff21823b7d9805fb
Lucas.Qiu_NAM <Lucas.Qiu@united-imaging.com>
2026-03-02 11:48:09 -0600

    Initial commit: React+Vite scaffold, docker-compose infra, reference screenshots

---
+++ feishu-clone/src/App.tsx
@@ -0,0 +1,19 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        {/* Left Sidebar Placeholder */}
+      </div>
+      <div className="chat-list">
+        {/* Chat List Placeholder */}
+      </div>
+      <div className="chat-window">
+        {/* Chat Window Placeholder */}
+      </div>
+    </div>
+  )
+}
+
+export default App;
